ac4949ec2f3087c0b62876788ccff508a1487676
Arikko Howell <arikkoh@gmail.com>
2015-07-11 22:27:57 -0700

    full project commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,20 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by arikko on 7/11/15.
+//  Copyright (c) 2015 arikko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// struct keyword, Capital "Struct" name, {} contain the properties the struct can have.
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+
+}
---
+++ LionsAndTigers/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  LionsAndTigers
+//
+//  Created by arikko on 7/10/15.
+//  Copyright (c) 2015 arikko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    var myTigers:[Tiger] = []
+    var currentIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var myTiger = Tiger()
+        myTiger.name = "Solo"
+        myTiger.breed = "Bengal"
+        myTiger.age = 3
+        myTiger.image = UIImage(named: "BengalTiger.jpg")
+        
+        myTigers.append(myTiger)
+        
+        println("My Tiger's name is: \(myTiger.name), its' age is \(myTiger.age), it's breed is \(myTiger.breed) and it's image is \(myTiger.image)")
+        
+        myImageView.image = myTiger.image
+        nameLabel.text = myTiger.name
+        ageLabel.text = "\(myTiger.age)"
+        breedLabel.text = myTiger.breed
+        
+        var secondTiger = Tiger()
+        secondTiger.name = "Lea"
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.age = 2
+        secondTiger.image = UIImage(named: "IndochineseTiger.jpg")
+        
+        var thirdTiger = Tiger()
+        thirdTiger.name = "Yoda"
+        thirdTiger.breed = "Malayan Tiger"
+        thirdTiger.age = 4
+        thirdTiger.image = UIImage(named: "MalayanTiger.jpg")
+        
+        var fourthTiger = Tiger()
+        fourthTiger.name = "Luke"
+        fourthTiger.breed = "Siberian Tiger"
+        fourthTiger.age = 5
+        fourthTiger.image = UIImage(named: "SiberianTiger.jpg")
+        
+        myTigers += [secondTiger, thirdTiger, fourthTiger]
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+        var randomIndex:Int
+        
+        // Do loop to make sure a different tiger from the current one is shown each time button is pressed
+        
+        do {
+        
+        randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
+        
+        } while currentIndex == randomIndex
+        
+        let tiger = myTigers[randomIndex]
+        
+        UIView.transitionWithView(self.view, duration: 2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
+            
+            self.myImageView.image = tiger.image
+            self.nameLabel.text = tiger.name
+            self.ageLabel.text = "\(tiger.age)"
+            self.breedLabel.text = tiger.breed
+            
+            }, completion: {
+            (finished: Bool) -> () in
+        })
+        
+        // Set the random integer dynamically based on number of elements in the array
+
+        
+//        myImageView.image = tiger.image
+//        nameLabel.text = tiger.name
+//        ageLabel.text = "\(tiger.age)"
+//        breedLabel.text = tiger.breed
+    }
+
+}
+
